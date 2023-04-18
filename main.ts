1818814b50eda6c2008449a8e13109b12d117cb0
donfii <130453085+donfii@users.noreply.github.com>
2023-04-18 11:59:16 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    radio.sendString("smile")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showIcon(IconNames.Happy)
+})
+radio.setGroup(69)
+basic.forever(function () {
+	
+})
